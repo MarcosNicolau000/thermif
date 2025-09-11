16b7db30abf318ad0cbebeeaecac98987be8fc5a
MarcosNicolau000 <marcosnicolauoliveira@outlook.com>
2025-09-11 14:56:27 -0300

    (build) Microcontroller and Dashboard directories created

---
+++ dashboard/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1>Therm IF</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
