63a2bfaaffb1c7fc122f0f96d3c945b8003d9ee3
MarcosNicolau000 <marcosnicolauoliveira@outlook.com>
2025-09-18 08:40:44 -0300

    style: Added  responsiveness to measurement card info

--- dashboard/src/components/MeasurementCards.tsx
+++ dashboard/src/components/MeasurementCards.tsx
@@ -1,26 +1,52 @@
+
+import Image from "next/image";
 let temperatura = 35;
 let umidade = 20;
 
 export default function MeasurementCard() {
   return (
     <>     
-        <div className="logo-container flex justify-center">
-            <img className="logo" src="/thermif-logo.png" alt="ThermIF Logo" />
+    <div className="logo-container flex justify-center">
+      <Image
+        src="/thermif-logo.png"
+        alt="ThermIF Logo"
+        width={400}
+        height={120}
+        className="logo object-contain m-20 w-1/2 md:w-4/10 lg:w-3/10 xl:w-2/10"
+        priority
+      />
         </div>
-        <div className="card flex flex-row justify-around mt-10 p-10">
-            <div className="measurement">
-                <span className="text-6xl">{temperatura}°C</span>
-                <img src="/hot-temperature-icon.png" alt="Temperature Icon" className="inline-block w-16 h-16 ml-5"/>
-                <p>Temperatura Instantânea</p>
+        <div className="card mb-20 flex flex-col items-center justify-around sm:flex-row">
+            <div className="measurement flex flex-col mt-20">
+                <div className="main-info flex items-center">
+                    <span className="text-6xl">{temperatura} °C</span>
+                    <Image
+                      src="/hot-temperature-icon.png"
+                      alt="Temperature Icon"
+                      width={300}
+                      height={300}
+                      className="inline-block w-16 h-16 ml-5 top-200"
+                      priority
+                    />
+                </div>
+                <p className="w-full text-justify text-lg mt-2">Temperatura Instantânea</p>
             </div>
-            <div className="measurement">
-                <span className="text-6xl">{umidade}%</span>
-                <img src="/humidity-icon.png" alt="Humidity Icon" className="inline-block w-16 h-16 ml-5"/>
-                <p>Umidade Relativa do Ar</p>
+            <div className="measurement flex flex-col mt-20">
+                <div className="main-info flex items-center">
+                    <span className="text-6xl">{temperatura} %</span>
+                    <Image
+                      src="/humidity-icon.png"
+                      alt="Temperature Icon"
+                      width={300}
+                      height={300}
+                      className="inline-block w-16 h-16 ml-5 top-200"
+                      priority
+                    />
+                </div>
+                <p className="w-full text-justify text-lg mt-2">Umidade Relativa do Ar</p>
             </div>
         </div>
             
-        
     </>
   );
 }
