7f5ca5d98f74b6045d935dd51e10fa79a14a84dc
MarcosNicolau000 <marcosnicolauoliveira@outlook.com>
2025-10-02 07:40:19 -0300

    build: gitignore update

--- microcontroller/ThermIF/src/main.cpp
+++ microcontroller/ThermIF/src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+/* #include <Arduino.h>
 
 #define ENABLE_USER_AUTH
 #define ENABLE_DATABASE
@@ -127,4 +127,42 @@ void lcdPrintInfo(float humidity, float temperature ) {
   lcd.print(temperature);
   lcd.print((char)223); 
   lcd.print("C"); 
+} */
+
+
+#include "DHT.h"
+ 
+#define DHTPIN 23 
+#define DHTTYPE DHT11 // DHT 11
+ 
+
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();
+}
+ 
+void loop() 
+{
+ 
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Falha na leitura do sensor DHT");
+  } 
+  else
+  {
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print("  ");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+  delay(1000);
 }
